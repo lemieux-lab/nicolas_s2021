77da55620fd8bc86d7a1c2b3527fe45d531c0afe
Nicolas Jacquin <36972435+Gortaf@users.noreply.github.com>
2021-05-28 19:54:47 -0400

    Changed loss tracking and result testing functions
    Changed loss tracking and result testing functions to be more gpu friendly.
    Also removed a few useless imports

--- kmer_oracle.jl
+++ kmer_oracle.jl
@@ -1,15 +1,31 @@
 using CUDA
 using Flux
-using FileIO
 using Gadfly
 using ProgressBars
 using Statistics
 using HDF5
 using Cairo
-using Tullio
 
 include("kmer_utils.jl")
 CUDA.allowscalar(false)
+
+function evaluate_loss(loss::Function, kmers::Array{Bool, 2}, counts::Array{Int32, 1}; device::Function=cpu)
+    dl = Flux.Data.DataLoader((kmers, counts), batchsize=1000)
+    total = 0
+    for (i, e) in ProgressBar(dl)
+        total+=sum(loss(device(i), device(e)))
+    end
+    return total
+end
+
+function evaluate_results(network, testing_set::Array{Bool, 2}; device::Function=cpu)
+    o_test = Array{Float64, 1}[]
+    dl = Flux.Data.DataLoader(kmers, batchsize=1000)
+    for i in ProgressBar(dl)
+        push!(o_test, network(device(i)))
+    end
+    return o_test
+end
 
 function train_and_plot()
     # device = cpu
@@ -39,14 +55,19 @@ function train_and_plot()
     # @time i_train, e_train = parse_saved_onehot_kmers(training_df[!, "kmers"]), training_df[!, "counts"]
     # @time i_test, e_test = parse_saved_onehot_kmers(testing_df[!, "kmers"]), testing_df[!, "counts"]
 
-    i_train |> device
-    e_train |> device
-    i_test |> device
-    e_test |> device
+    # i_train |> device
+    # e_train |> device
+    # i_test |> device
+    # e_test |> device
 
     network = neural_network() |> device
-    loss(input, expected) = Flux.Losses.mse(network(device(input)), device(expected))
-    dl_train = Flux.Data.DataLoader((i_train, e_train), batchsize=1, shuffle=true)
+    loss(input, expected) = Flux.Losses.mse(network(input), expected)
+    # function loss(input, expected)
+    #     println("input:", input)
+    #     l = Flux.Losses.mse(network(input), expected)
+    #     return l
+    # end
+    dl_train = Flux.Data.DataLoader((i_train, e_train), batchsize=1000, shuffle=true)
     opt = Flux.ADAM()
     ps = Flux.params(network)
 
@@ -54,24 +75,26 @@ function train_and_plot()
     losses = Int64[]
     testing_losses = Int64[]
 
+    # println(loss.(eachcol(i_train), eachcol(e_train)))
+
     # Main training loop
-    epochs = 10
+    epochs = 2
     cur_loss = "N/A"
     iter = ProgressBar(1:epochs)
     for i in iter
         set_postfix(iter, Loss=cur_loss)
         for (i, e) in ProgressBar(dl_train)
             gs = gradient(params(network)) do
-                loss(i, e[1])
+                loss(device(i), device(e))
             end 
             Flux.Optimise.update!(opt, ps, gs)
         end
-        cur_loss = sum(loss.(i_train, e_train))
+        cur_loss = evaluate_loss(loss, i_train, e_train, device=device)
         push!(losses, cur_loss)
-        push!(testing_losses, sum(loss.(i_test, e_test)))
+        push!(testing_losses, evaluate_loss(loss, i_test, e_test, device=device))
     end
 
-    o_test = [network(device(i)) for i in ProgressBar(i_test)]
+    o_test = evaluate_results(network, i_test, device=device)
 
     # Making a plot to visualise results
     set_default_plot_size(30cm, 30cm)
@@ -82,7 +105,7 @@ function train_and_plot()
     plot(x=hcat(e_test...), y=hcat(o_test...), Geom.point,
         Guide.xlabel("expected"), Guide.ylabel("obtained"))
     )
-    draw(PNG("/u/jacquinn/graphs/results_13H107_min-5_ALL_epoch-10.png", 50cm, 50cm), results)
+    draw(PNG("/u/jacquinn/graphs/results_13H107_min-5_ALL_epoch-2.png", 50cm, 50cm), results)
 
 end
 
