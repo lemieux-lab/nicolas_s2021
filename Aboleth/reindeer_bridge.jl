7713f5fefe779e1ed0d1568cbb3c5fe077fd8fd5
Nicolas Jacquin <36972435+Gortaf@users.noreply.github.com>
2021-08-05 19:39:26 -0400

    Add files via upload

--- Aboleth/reindeer_bridge.jl
+++ Aboleth/reindeer_bridge.jl
@@ -4,7 +4,21 @@ function load_reindeer_index(reindeer_dir::String)
                  Cvoid, (Cstring, ), reindeer_dir)
 end
 
-function query_on_loaded_index(query_path::String)
+function query_on_loaded_index(query_path::String, query_receptacle::Array{Int32, 1})
     ccall((:query_on_loaded_index, "/u/jacquinn/reindeer/REINDEER/reindeer.so"), 
-           Cvoid, (Cstring, ), query_path)
+           Cvoid, (Cstring, Ptr{Cint}), query_path, query_receptacle)
 end
+
+load_reindeer_index("/home/golem/scratch/jacquinn/data/reindeer_files/3_index/output_reindeer")
+to_build = Array{Int32, 1}(undef, 3)
+query_on_loaded_index("/home/golem/scratch/jacquinn/data/1_kmer.FASTA", to_build)
+println(to_build)
+
+# function test_table(to_add::Int64, table::Array{Int32, 1})
+#     ccall((:test_table, "/u/jacquinn/reindeer/REINDEER/reindeer.so"), 
+#            Cvoid, (Cint, Ptr{Cint}), to_add, table)
+# end
+
+# table = Array{Int32, 1}(undef, 10)
+# test_table(42, table)
+# println(table)
